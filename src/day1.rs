075743c28e1fd7660226ff18b30ae237d0ae593b
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2023-11-30 21:36:29 +0100

    Initial commit -  2

---
+++ src/day1.rs
@@ -0,0 +1,16 @@
+use crate::input::*;
+
+pub fn day1() {
+    println!("Day 1");
+    part1();
+    part2();
+}
+
+fn part1() {
+    println!("Part 1");
+    let test_input = lines_from_day(1);
+}
+
+fn part2() {
+    println!("Part 2")
+}
