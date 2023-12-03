3672ba5635cc4040cf664f0a0d5178b5db173992
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2023-12-03 18:22:42 +0100

    Prepare day4

---
+++ src/day4.rs
@@ -0,0 +1,41 @@
+use crate::input::*;
+
+pub fn day4() {
+    println!("Day 4");
+    let input = lines_from_day(4);
+    part1(input.clone());
+    part2(input.clone());
+}
+
+fn part1(_input: Vec<String>) {
+    println!("Part 1");
+
+    let _test_input = vec![
+        "1abc2".to_string(),
+        "pqr3stu8vwx".to_string(),
+        "a1b2c3d4e5f".to_string(),
+        "treb7uchet".to_string(),
+    ];
+
+    let output: u32 = 0;
+    println!("Test: {}", output);
+    println!("Output: {}", output);
+}
+
+fn part2(_input: Vec<String>) {
+    println!("Part 2");
+    let _test_input = vec![
+        "two1nine".to_string(),
+        "eightwothree".to_string(),
+        "abcone2threexyz".to_string(),
+        "xtwone3four".to_string(),
+        "4nineeightseven2".to_string(),
+        "zoneight234".to_string(),
+        "7pqrstsixteen".to_string(),
+    ];
+
+    let output: u32 = 0;
+
+    println!("Test: {}", output);
+    println!("Output: {}", output);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use std::env;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod input;
 
 use day1::day1;
 use day2::day2;
 use day3::day3;
+use day4::day4;
 
 fn main() {
     println!("Hello, AOC world!");
@@ -19,6 +21,7 @@ fn main() {
         1 => day1(),
         2 => day2(),
         3 => day3(),
+        4 => day4(),
         d => println!("No solution file yet for day {d}"),
     }
 }
