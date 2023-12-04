3d903f01d6e92291a3b45c0bc92b326fcb44c217
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2023-12-04 11:17:34 +0100

    Prepare day5

---
+++ src/day5.rs
@@ -0,0 +1,42 @@
+use crate::input::lines_from_day;
+
+pub fn day4() {
+    println!("Day 5");
+    let input = lines_from_day(5);
+    part1(input.clone());
+    part2(input.clone());
+}
+
+fn part1(_input: Vec<String>) {
+    println!("Part 1");
+
+    let _test_input = vec![
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53".to_string(),
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19".to_string(),
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1".to_string(),
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83".to_string(),
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36".to_string(),
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".to_string(),
+    ];
+
+    let output: i32 = 0;
+    println!("Test: {}", output);
+    println!("Output: {}", output);
+}
+
+fn part2(_input: Vec<String>) {
+    println!("Part 2");
+
+    let _test_input = vec![
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53".to_string(),
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19".to_string(),
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1".to_string(),
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83".to_string(),
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36".to_string(),
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".to_string(),
+    ];
+
+    let output: i32 = 0;
+    println!("Test: {}", output);
+    println!("Output: {}", output);
+}
